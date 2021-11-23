b29a1e9efe1e74ed40b5fede3c4013c97e610cac
l3lackheart <dgbao1340@gmail.com>
2021-11-23 23:19:21 +0700

    correct period

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('raca:check-price --category=13 --category=15 --category=16 --category=17 --category=20')->quarterly();
-        $schedule->command('raca:check-price --category=13 --category=15 --category=16 --category=17 --category=20 --sort=single_price')->quarterly();
+        $schedule->command('raca:check-price --category=13 --category=15 --category=16 --category=17 --category=20')->everyFifteenMinutes();
+        $schedule->command('raca:check-price --category=13 --category=15 --category=16 --category=17 --category=20 --sort=single_price')->everyFifteenMinutes();
     }
 
     /**
