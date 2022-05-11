ad5ca906e8755cb2117c88ec6c21d377e052df2b
l3lackheart <dgbao1340@gmail.com>
2022-05-11 17:24:05 +0700

    fancybox for markdown view

--- Modules/Core/Resources/views/components/visual/markdown.blade.php
+++ Modules/Core/Resources/views/components/visual/markdown.blade.php
@@ -1,5 +1,9 @@
 @props(['content'])
 
-<div {{ $attributes->class(['prose']) }}>
+@php
+$id = $attributes->get('id') ?? Str::random(10);
+@endphp
+
+<div {{ $attributes->class(['prose']) }} id="{{ $id }}" x-data x-init="Fancybox.bind('#{{ $id }} img', { groupAll: true })">
     {!! \Illuminate\Support\Str::of($content)->markdown() !!}
 </div>
