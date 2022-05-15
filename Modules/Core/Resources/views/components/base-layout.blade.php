6f3995988f69ad5e143711b3a3d13c74e1060dc9
l3lackheart <dgbao1340@gmail.com>
2022-05-15 13:02:36 +0700

    add missing turbolinks adapter

--- Modules/Core/Resources/views/components/base-layout.blade.php
+++ Modules/Core/Resources/views/components/base-layout.blade.php
@@ -25,6 +25,8 @@
     <x-core::notification />
     {{-- @livewire('livewire-ui-spotlight') --}}
     @livewireScripts
+    <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js"
+        data-turbolinks-eval="false" data-turbo-eval="false"></script>
 </body>
 
 </html>
