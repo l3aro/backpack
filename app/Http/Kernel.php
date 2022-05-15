6263462690d1c206eea63306eb0d846d65081991
l3lackheart <dgbao1340@gmail.com>
2022-05-15 10:48:08 +0700

    add pagespeed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \RenatoMarinho\LaravelPageSpeed\Middleware\RemoveComments::class,
+            \RenatoMarinho\LaravelPageSpeed\Middleware\CollapseWhitespace::class,
         ],
 
         'api' => [
