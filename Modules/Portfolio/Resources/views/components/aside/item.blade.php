df2e67da3ca2691ded648cff8c8f075214151325
l3aro <dgbao1340@gmail.com>
2022-07-09 22:24:45 +0700

    config behavior aside link

--- Modules/Portfolio/Resources/views/components/aside/item.blade.php
+++ Modules/Portfolio/Resources/views/components/aside/item.blade.php
@@ -1,8 +1,9 @@
 @props(['active'])
 
 <li @class([
-    'mb-4 p-0 font-bold text-neutral-400 hover:text-black transition',
+    'mb-4 p-0 font-bold transition',
     'text-black dark:text-white' => $active,
+    'text-neutral-400 hover:text-black dark:hover:text-white' => !$active,
 ])>
     <a {{ $attributes->class(['border-b-2 border-b-black dark:border-b-white/80 pb-1' => $active]) }}>{{ $slot }}</a>
 </li>
