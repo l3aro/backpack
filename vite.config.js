e20d09a0e14ec557a43eb43bdc530ff63a2078d7
l3aro <dgbao1340@gmail.com>
2022-08-11 21:31:26 +0700

    remove vite livewire plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import laravel from 'laravel-vite-plugin'
-import livewire, { defaultWatches } from '@defstudio/vite-livewire-plugin'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -8,9 +7,5 @@ export default defineConfig({
             'Modules/Core/Resources/assets/js/app.js',
             'Modules/Core/Resources/assets/css/app.css',
         ]),
-        livewire({
-            refresh: ['Modules/Core/Resources/assets/css/app.css'],
-            watch: [...defaultWatches, '**/Modules/**/Livewire/**/*.php'],
-        }),
     ],
 })
